f57f81644e3b242bd9f8303dff7a232a162d44b9
hitesh <hiteshasoriya10@gmail.com>
2023-04-08 10:08:33 +0530

    correction

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,11 +4,11 @@ import {getFirestore, collection} from'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_SECRET_KEY,
-  authDomain: process.env.DOMAIN,
+  authDomain: process.env.REACT_APP_DOMAIN,
   projectId: "movieworld-285a8",
   storageBucket: "movieworld-285a8.appspot.com",
   messagingSenderId: "150512753475",
-  appId: process.env.APPID
+  appId: process.env.REACT_APP_APPID
 };
 
 const app = initializeApp(firebaseConfig);
